cc0cf421dac0770638683e8b431075f93ca78388
xchaset <943314903@qq.com>
2020-04-05 01:02:48 +0800

    添加文档注释

--- excel-example/src/main/java/com/xchaset/excelexample/excel/utils/ExcelUtil.java
+++ excel-example/src/main/java/com/xchaset/excelexample/excel/utils/ExcelUtil.java
@@ -14,8 +14,12 @@ import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFPivotTable;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -54,33 +58,44 @@ public class ExcelUtil {
         writer.finish();
     }
 
+
     /**
      * poi 透视表的创建
+     * @param filePath 原始数据文件路径
+     * @param outFilePath 输出文件路径
      */
     public static void createPivotTable(String filePath,String outFilePath){
         File file = new File(filePath);
-        XSSFWorkbook workbook = null;
+        // 创建XSSFWorkbook
+        XSSFWorkbook workbook;
         try (FileInputStream fileInputStream = new FileInputStream(file);){
-            workbook = new XSSFWorkbook(fileInputStream);
+             workbook = XSSFWorkbookFactory.createWorkbook(fileInputStream);
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
         // 获取透视表数据源
         XSSFSheet buyerReview = workbook.getSheet("Review");
+        // 创建一个新的sheet也。用于存放透视表
         XSSFSheet overallReport = workbook.createSheet("pivot sheet");
+        // 获取透视表的最后一行行号
         int lastRowNum = buyerReview.getLastRowNum();
         // 透视表数据源数据区域
-        AreaReference area = new AreaReference("A1:K" + lastRowNum, SpreadsheetVersion.EXCEL97);
+        AreaReference area = new AreaReference("A1:K" + lastRowNum, SpreadsheetVersion.EXCEL2007);
         // 透视表显示起始位置
         CellReference position = new CellReference("A2");
 
         XSSFPivotTable pivotTable = overallReport.createPivotTable(area, position, buyerReview);
+        // 添加行标签
         pivotTable.addRowLabel(0);
         pivotTable.addRowLabel(8);
+
+        // 添加列标签,行标签会横向展示
         pivotTable.addColLabel(10);
+        // 添加行数据统计函数
         pivotTable.addColumnLabel(DataConsolidateFunction.COUNT,10,"FieldName");
 
+        // 写出到文件
         try(FileOutputStream fileOutputStream = new FileOutputStream(new File(outFilePath))){
             workbook.write(fileOutputStream);
         } catch (IOException e) {
